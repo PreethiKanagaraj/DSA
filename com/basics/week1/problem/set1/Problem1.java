2d356ec22d8bc784f686bff8bb2fba5dfa0e5701
PreethiKanagaraj <55791796+PreethiKanagaraj@users.noreply.github.com>
2025-05-11 17:47:35 +0530

    set1-Problem1

---
+++ com/basics/week1/problem/set1/Problem1.java
@@ -0,0 +1,19 @@
+package com.basics.week1.problem.set1;
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		System.out.println(num);
+		String name = sc.next();
+		System.out.println(name);
+		String letter = sc.next();
+		System.out.println(letter);
+	}
+
+}
