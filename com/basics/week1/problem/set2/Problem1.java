253507a67c9749660307b39ad5575dc925f6deb6
PreethiKanagaraj <55791796+PreethiKanagaraj@users.noreply.github.com>
2025-05-11 17:52:15 +0530

    Create Problem1.java

---
+++ com/basics/week1/problem/set2/Problem1.java
@@ -0,0 +1,18 @@
+package com.basics.week1.problem.set2;
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+
+		for (int i = 1; i <= y; i++) {
+			System.out.println(x);
+		}
+
+	}
+
+}
